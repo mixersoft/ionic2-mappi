afb3f06cb1b4b5b2a6ea46940856d1527eb0bab8
michael lin <michael@snaphappi.com>
2016-09-29 00:34:35 +0300

    add `com-snaphappi-plugin-camera-roll-location` to project

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,11 +2,42 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+
+declare var cordova: any;
+
 @Component({
   templateUrl: 'home.html'
 })
 export class HomePage {
+  items : any[] = [];
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  clear (){
+    this.items = [];
+  }
+
+  handleClick (){
+    if (window && !window['cordova']){
+      this.items = ["hello", "world"];
+      return console.warn("cordova not available");
+    }
+    if (!cordova.plugins.CameraRollLocation)
+      return console.warn("CameraRollLocation not available");
+    const plugin = cordova.plugins.CameraRollLocation;
+    const getByMomentsOptions = {
+      from: new Date('2010-09-01'),
+      to: new Date('2016-09-30')
+    }
+    plugin.getByMoments(getByMomentsOptions)
+    .then( (result)=>{
+      // BUG: this is not updating the view in angular2
+      this.items = result
+      console.info(`plugin getByMoments() result[0]=${ this.items[0] }`);
+    }).catch( (err)=>console.log(err) )
+
+    console.info("handleClick");
   }
 }
