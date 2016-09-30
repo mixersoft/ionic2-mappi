8ab41e551c117fbc77f6cfb4b4b478fc83371e89
michael lin <michael@snaphappi.com>
2016-09-30 22:56:14 +0300

    import, extend `js-marker-clusterer` lib

---
+++ src/shared/map-google/mappi-marker-clusterer.ts
@@ -0,0 +1,44 @@
+/// <reference path="./mappi-marker-clusterer.d.ts" />
+import _ from "lodash";
+import { MarkerClusterer, ClusterIcon } from 'js-marker-clusterer';
+
+declare var google;
+
+export class MappiMarkerClusterer extends MarkerClusterer {
+  constructor(map: any, opt_markers?: any, opt_options?: any) {
+    super(map, opt_markers, opt_options);
+    const self = this;
+    self.triggerClustersChanged = _.debounce( ()=>{
+      console.log("debounced MarkerClusterer.triggerClustersChanged()");
+      self.triggerClustersChanged.call(self)
+    }, 5*1000)
+  }
+  getClusters(){
+    return this.clusters_;
+  }
+  addMarkers(markers, opt_nodraw) {
+    super.addMarkers(markers, opt_nodraw)
+    this.triggerClustersChanged()
+  }
+  removeMarker(marker, opt_nodraw) {
+    const removed = super.removeMarker(marker, opt_nodraw);
+    if (removed) this.triggerClustersChanged();
+    return removed;
+  }
+  removeMarkers(markers, opt_nodraw) {
+    const removed = super.removeMarkers(markers, opt_nodraw);
+    if (removed) this.triggerClustersChanged();
+    return removed;
+  }
+  triggerClustersChanged(){
+    google.maps.event.trigger(this.map_, 'clustersChanged', this.clusters_);
+  }
+}
+
+// we need to override the method in the base class
+ClusterIcon.prototype.triggerClusterClick0 = ClusterIcon.prototype.triggerClusterClick;
+ClusterIcon.prototype.triggerClusterClick = ()=>{
+  ClusterIcon.prototype.triggerClusterClick0()
+  // added
+  google.maps.event.trigger(this.map_, 'clusterclick', this.cluster_);
+}
